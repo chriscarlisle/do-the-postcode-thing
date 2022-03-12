3b4c4729d3947478f34fae165a51f424ee33922d
Chris Carlisle <chris@chriscarlisle.uk>
2022-03-12 11:44:53 +0000

    Add support for explicity serviced postcodes

--- app/models/postcode_check.rb
+++ app/models/postcode_check.rb
@@ -1,17 +1,28 @@
 class PostcodeCheck
   class PostcodeCheckUnavailable < StandardError; end
 
+  EXPLICITLY_SERVICED_POSTCODES = [
+    "SH24 1AA",
+    "SH24 1AB"
+  ]
+
   def initialize(postcode)
     @ukpostcode = UKPostcode.parse(postcode)
   end
 
   def serviced?
-    return valid_uk_postcode_format?
+    return false unless valid_uk_postcode_format?
+
+    return explicitly_serviced_postcode?
   end
 
   private
 
   def valid_uk_postcode_format?
     @ukpostcode.valid?
+  end
+
+  def explicitly_serviced_postcode?
+    EXPLICITLY_SERVICED_POSTCODES.include? @ukpostcode.to_s
   end
 end
--- test/models/postcode_check_test.rb
+++ test/models/postcode_check_test.rb
@@ -1,22 +1,33 @@
 require "test_helper"
 
 class PostcodeCheckTest < ActiveSupport::TestCase
-  VALID_FORMAT_POSTCODES = [
-    "SE1 7QD",
-    "SE17QA",
-    "sh24 1aa",
-    "sh241aa"
-  ]
+  VALID_FORMAT_POSTCODES = {
+    explicitly_serviced: [
+      "SH24 1AA",
+      "sh241ab"
+    ],
+    not_serviced: [
+      "LA2 6PR",
+      "la26ps",
+    ]
+  }
 
   INVALID_FORMAT_POSTCODE = "Banana"
-
-  test "serviced? returns true for postcodes in a valid format" do
-    VALID_FORMAT_POSTCODES.each do |postcode|
-      assert PostcodeCheck.new(postcode).serviced?
-    end
-  end
 
   test "serviced? returns false for postcodes in an invalid format" do
     assert !PostcodeCheck.new(INVALID_FORMAT_POSTCODE).serviced?
   end
+
+  test "serviced? returns false for postcodes in a valid format, but aren't serviced" do
+    VALID_FORMAT_POSTCODES[:not_serviced].each do |postcode|
+      assert !PostcodeCheck.new(postcode).serviced?
+    end
+  end
+
+  test "serviced? returns true for explicitly serviced postcodes" do
+    VALID_FORMAT_POSTCODES[:explicitly_serviced].each do |postcode|
+      assert PostcodeCheck.new(postcode).serviced?
+    end
+  end
+
 end
